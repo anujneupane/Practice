e1edebb33086051230902bdab8e422584a9b930a
anujneupane <anujneupane987@gmail.com>
2024-09-29 19:26:58 +0545

    manual form order

--- course/forms.py
+++ course/forms.py
@@ -1,4 +1,4 @@
 from django import forms 
 class StudentsRegis(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(widget=forms.TextInput(attrs={'id':'form-control'}))
     email = forms.EmailField()
