1b00f45449f51688e9b4b4b44bae2d57bc7aa2d8
anujneupane <anujneupane987@gmail.com>
2024-09-30 14:07:22 +0545

    added password field from widget

--- course/forms.py
+++ course/forms.py
@@ -7,7 +7,7 @@ class StudentsRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     mobile= forms.IntegerField()
-    key = forms.CharField(widget=forms.HiddenInput())
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
 
